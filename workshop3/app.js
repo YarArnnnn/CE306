3224322b9e0afac10065546863b896e9b8643f76
Wisarut <66111966@dpu.ac.th>
2025-08-28 11:44:05 +0700

    upload old

---
+++ workshop3/app.js
@@ -0,0 +1,92 @@
+// function testVar() {
+//     var v = 10;
+//     if (true) {
+//         var v = 20;
+//         console.log(v);
+//     }
+//     console.log(v);
+// }
+
+// var v =10;
+// v =20;
+// console.log(v);
+
+// let l =10;
+// l = 20;
+// console.log(l);
+
+// const c = 10;
+// c = 20;
+// console.log(c);
+
+
+// function testLet() {
+//     let l = 10;
+//     if (true) {
+//         let l = 20;
+//         console.log(l);
+//     }
+//     console.log(l);
+// }
+
+// function testConst() {
+//     const c = 10;
+//     if (true) {
+//         const c = 20;
+//         console.log(c);
+//     }
+//     console.log(c);
+// }
+// testVar();
+// console.log();
+// testLet();
+// console.log();
+// testConst();
+// console.log();
+
+// let massage ="Hello world";
+// let score = 90;
+// let isPass = true;
+// let empthyValue = null;
+// let notDifine;
+// let person = {name:"alice", age: 25 };
+// let number = [1, 2, 3 ,4]
+
+// console.log(typeof massage);
+// console.log(typeof notDifine);
+
+// let age = 20;
+// let isAdult = (age >=18) && (age < 65);
+// let isChild = (age < 18) || (age > 65);
+// let isnotAdult = !isAdult
+// console.log()
+
+// let score = 50;
+// let grade = (score >=80) ? "A" : "B";
+// console.log("Grade: ",grade);
+
+// let score =75;
+
+// if (score >= 80){
+//     console.log("grade A");
+// } else if (score >= 70) {
+//     console.log("grade B");
+// } else if (score >= 60) {
+//     console.log("grade C");
+// } else if (score >= 50) {
+//     console.log("grade D")
+// } else  {
+//     console.log("grade F")
+// }
+
+// let j =1;
+// while (j <=5) {
+//     console.log(j);
+//     j++;
+// }
+
+// let fruits = ["apple", "banannana", "cherry"];
+// for (let i = 0; i< fruits.length; i++) {
+//     console.log(`Fruit ${i+1}: ${fruits[i]}`);
+// }
+
---
+++ workshop3/workshop3.js
@@ -0,0 +1,23 @@
+// workshop 3.1
+let score = 85;
+if (score >= 80){
+    console.log("grade A");
+}
+if (score <= 79 && score >=70 ) {
+    console.log("grade B");
+}
+if (score <= 69 && score >=60){
+    console.log("grade C");
+}
+if (score <= 59 && score >=50 ) {
+    console.log("grade D");
+}
+if (score <= 49){
+    console.log("grade F");
+}
+// workshop3.2
+let productPrice = 1000;
+let discountPer = 10;
+let discounted = productPrice / discountPer;
+
+if ()
