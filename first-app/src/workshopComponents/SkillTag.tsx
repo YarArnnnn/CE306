479680004dd591d85b20c7157a56a40cf2d384b6
Wisarut <66111966@dpu.ac.th>
2025-09-04 11:56:48 +0700

    homeworkSetup

---
+++ first-app/src/workshopComponents/SkillTag.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface props {
+    skillName: string;
+    level?: 'Beginer' | 'Intermediate' | 'Advance';
+    
+}
---
+++ first-app/src/workshopComponents/userProfileCard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface userData {
+    name: string;
+    id: 'string' | 'number';
+    avatarUrl?:'string' | 'www.pornhub.com'
+    email: string;
+    isOnline: boolean;
+    skills: Array<{name: string;level?:'Beginer' | 'Intermediate' | 'Advance'}>;
+
+}
+
+const onViewDetails {
+    
+}
