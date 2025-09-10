bb509ff66637d1919700df1c0161e5278404aba8
Wisarut <66111966@dpu.ac.th>
2025-09-10 22:28:27 +0700

    checkpoint1

--- first-app/src/App.tsx
+++ first-app/src/App.tsx
@@ -1,18 +1,35 @@
-import React from "react";
-import Greeting from "./Components/Greeting";
-import WelcomeBanner from "./Components/WelcomeBanner"; 
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div>
-      <Greeting name="ผู้ใช้งาน TypeScript" messageCount={5} />
-      <Greeting name="นักพัฒนา Vite" />
+  const [count, setCount] = useState(0)
 
-      <WelcomeBanner title="ยินดีต้อนรับสู่โลกของ TypeScript!">
-        <p>เนื้อหาส่วนนี้ส่งผ่าน children prop</p>
-      </WelcomeBanner>
-    </div>
-  );
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
